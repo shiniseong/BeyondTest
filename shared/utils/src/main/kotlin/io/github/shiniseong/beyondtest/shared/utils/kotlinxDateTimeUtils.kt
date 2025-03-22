9e21d208d779498198d8ccf44d2ade897e58235a
shiniseong <hss275989@gmail.com>
2025-03-22 13:47:28 +0900

    feat: kotlinxDateTimeUtils 작성. LocalDateTime.now() 확장 함수 작성.

---
+++ shared/utils/src/main/kotlin/io/github/shiniseong/beyondtest/shared/utils/kotlinxDateTimeUtils.kt
@@ -0,0 +1,8 @@
+package io.github.shiniseong.beyondtest.shared.utils
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+fun LocalDateTime.now() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
