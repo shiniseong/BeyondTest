0d14f24823eff2ef25beb28f8f2676fdfcb2b667
shiniseong <hss275989@gmail.com>
2025-03-23 18:38:40 +0900

    chore: add dependency management

--- services/prescription-service/adapter/inbound/grpc/build.gradle.kts
+++ services/prescription-service/adapter/inbound/grpc/build.gradle.kts
@@ -2,13 +2,23 @@ plugins {
     // Apply the shared build logic from a convention plugin.
     // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     id("buildsrc.convention.kotlin-jvm")
+    alias(libs.plugins.protobuf)
+    alias(libs.plugins.springKotlin)
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDependencyManagement)
 }
 
 dependencies {
     api(project(":services:prescription-service:application"))
 
-    dependencies {
+    implementation(libs.grpc.services)
+    implementation(libs.spring.grpc.spring.boot.starter)
 
-        testImplementation(libs.bundles.testEcosystem)
+    testImplementation(libs.bundles.testEcosystem)
+    testImplementation(libs.spring.grpc.test)
+}
+dependencyManagement {
+    imports {
+        mavenBom(libs.spring.grpc.dependencies.get().toString())
     }
 }
