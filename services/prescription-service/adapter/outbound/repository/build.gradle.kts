4f479c0e4faed7d3c131db4fe88ff6779db19c75
shiniseong <hss275989@gmail.com>
2025-03-23 11:43:29 +0900

    feat: 처방코드 서비스 모듈의 영속성 레이어에 mysql과 h2 의존성 추가.

---
+++ services/prescription-service/adapter/outbound/repository/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+    alias(libs.plugins.springKotlin)
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDependencyManagement)
+}
+
+dependencies {
+    api(project(":services:prescription-service:application"))
+
+    dependencies {
+        implementation(libs.spring.boot.starter.data.r2dbc)
+        runtimeOnly(libs.r2dbc.mysql)
+        runtimeOnly(libs.mysql.connector)
+        testRuntimeOnly(libs.r2dbc.h2)
+        testRuntimeOnly(libs.h2.database)
+        testImplementation(libs.bundles.testEcosystem)
+    }
+}
