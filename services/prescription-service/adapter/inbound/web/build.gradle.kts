c25b21a5d561e70f16a7beb53aeb7fde0f332611
shiniseong <hss275989@gmail.com>
2025-03-23 21:46:07 +0900

    feat: webflux 의존성 설정.

---
+++ services/prescription-service/adapter/inbound/web/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+    alias(libs.plugins.kotlinPluginSerialization)
+    alias(libs.plugins.springKotlin)
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDependencyManagement)
+
+    id(libs.plugins.javaTestFixtures.get().toString())
+}
+
+dependencies {
+    implementation(project(":services:prescription-service:application"))
+
+    api(libs.spring.boot.starter.webflux)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.reactor.kotlin.extensions)
+    implementation(libs.kotlinReflect)
+    implementation(libs.kotlinx.coroutines.reactor)
+
+    testImplementation(testFixtures(project(":services:prescription-service:application")))
+    testImplementation(libs.bundles.testEcosystem)
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.reactor.test)
+}
