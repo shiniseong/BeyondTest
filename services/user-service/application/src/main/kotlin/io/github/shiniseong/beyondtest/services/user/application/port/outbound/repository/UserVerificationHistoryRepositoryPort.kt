497f7bd403deeda826319608468ab52ecdad6b53
shiniseong <hss275989@gmail.com>
2025-03-24 05:13:15 +0900

    feat: UserVerificationHistoryRepositoryPort 정의

---
+++ services/user-service/application/src/main/kotlin/io/github/shiniseong/beyondtest/services/user/application/port/outbound/repository/UserVerificationHistoryRepositoryPort.kt
@@ -0,0 +1,8 @@
+package io.github.shiniseong.beyondtest.services.user.application.port.outbound.repository
+
+import io.github.shiniseong.beyondtest.services.user.domain.entity.UserVerificationHistory
+
+interface UserVerificationHistoryRepositoryPort {
+    suspend fun insert(userVerificationHistory: UserVerificationHistory): UserVerificationHistory
+    suspend fun getAll(): List<UserVerificationHistory>
+}
